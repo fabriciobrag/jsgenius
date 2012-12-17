8dd4318f9db8396a2bd87ad0c2302cd7314d304a
fabriciobrag <fabriciobedeschi@gmail.com>
2012-12-17 20:22:00 -0200

    DS_Store ignore

--- static/js/js.js
+++ static/js/js.js
@@ -1,6 +1,21 @@
 (function () {
+	
 	var sequence = [];
-
+	
+	// random = Math.floor(Math.random() * 4) + 1;
+	
+	document.getElementById("js_start").onclick = function (event) {
+		
+		startGame();
+		event.stopPropagation();
+		return false;
+	};
+	
+	
+	startGame = function  () {
+		
+	}
+	
 	
 
 
